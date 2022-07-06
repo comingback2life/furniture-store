99bde97aa9ce2620f207d05ce4960b535742c2e1
Samip <callmesamip@gmail.com>
2022-07-06 15:21:09 +1000

    Removed react console error and added the dev script in backend

--- admin-cms-frontend/src/App.js
+++ admin-cms-frontend/src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <p>React app </p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div>
+				<p>React app </p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
