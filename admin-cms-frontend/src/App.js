a8eea52a097927ba277d43ee1a848479983039b0
Samip <callmesamip@gmail.com>
2022-07-07 11:15:28 +1000

    Routes setup done

--- admin-cms-frontend/src/App.js
+++ admin-cms-frontend/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-
+import { Register } from './pages/register-login/Register';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
 	return (
-		<div className="App">
-			<div>
-				<p>React app </p>
-			</div>
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/admin/register" element={<Register />}></Route>
+					<Route path="*" element={<h2>404, Page Not Found</h2>}></Route>
+				</Routes>
+			</BrowserRouter>
+
+			{/**Register Page */}
 		</div>
 	);
 }
---
+++ admin-cms-frontend/src/pages/register-login/Register.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { DefaultLayout } from '../layouts/DefaultLayout';
+
+export const Register = () => {
+	return <DefaultLayout>Register</DefaultLayout>;
+};
