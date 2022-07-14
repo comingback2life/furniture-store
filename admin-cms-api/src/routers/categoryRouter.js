accaef5fcc18d3046839516c539ee594933c0b7d
Samip <callmesamip@gmail.com>
2022-07-14 12:40:41 +1000

    Added Slugify and updated the models

--- admin-cms-api/src/routers/categoryRouter.js
+++ admin-cms-api/src/routers/categoryRouter.js
@@ -1,15 +1,28 @@
 import express from 'express';
 import { newCategoryValidation } from '../middlewares/joi-validations/categoryValidation.js';
+import { insertCategory } from '../models/category/Category.models.js';
+import slugify from 'slugify';
 const router = express.Router();
 
-router.post('/', newCategoryValidation, (req, res, next) => {
+router.post('/', newCategoryValidation, async (req, res, next) => {
 	//newCategoryvalidation is validating if there are two properties that remain
 	try {
-		console.log(req.body);
-		res.json({
-			status: 'success',
-			message: 'Post Request to category has been received',
+		const slug = slugify(req.body.catName, {
+			lower: true,
+			trim: true,
 		});
+		req.body.catName = slug;
+		const result = await insertCategory(req.body);
+		console.log(result);
+		result?._id
+			? res.json({
+					status: 'success',
+					message: 'New Category has been added',
+			  })
+			: res.json({
+					status: 'error',
+					message: 'Category could not be added, please try again later',
+			  });
 	} catch (error) {
 		console.log(error);
 		error.status = 500;
