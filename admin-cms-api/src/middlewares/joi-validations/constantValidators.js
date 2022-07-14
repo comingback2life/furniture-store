4fc4d6c49a9862f2d03250f5b90c471b1c976a7a
Samip <callmesamip@gmail.com>
2022-07-14 11:56:26 +1000

    Working on validators

--- admin-cms-api/src/middlewares/joi-validations/constantValidators.js
+++ admin-cms-api/src/middlewares/joi-validations/constantValidators.js
@@ -1,1 +1,9 @@
 import Joi from 'joi';
+export const FNAME = Joi.string().alphanum().required().min(3).max(20);
+export const LNAME = Joi.string().alphanum().required().min(3).max(20);
+export const EMAIL = Joi.string().email({ minDomainSegments: 2 }).required();
+export const PHONE = Joi.string().required().min(10).max(15);
+export const DOB = Joi.date().allow(null);
+export const ADDRESS = Joi.string().allow(null).allow('');
+export const PASSWORD = Joi.string().required();
+export const REQUIREDSTR = Joi.string().required();
