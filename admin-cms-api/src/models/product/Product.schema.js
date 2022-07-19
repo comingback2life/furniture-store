05a867e2ae6c00b03e766e2128aa52799843ae12
Samip <callmesamip@gmail.com>
2022-07-19 10:59:04 +1000

    Writing models and schema for the products

---
+++ admin-cms-api/src/models/product/Product.schema.js
@@ -0,0 +1,69 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema(
+	{
+		status: {
+			type: String,
+			required: true,
+			default: 'inactive',
+		},
+		name: {
+			type: String,
+			required: true,
+			maxLength: [
+				100,
+				'Name of a product cannot be no longer than 100 characters',
+			],
+		},
+		SKU: {
+			type: String,
+			unique: true,
+			index: 1,
+			required: true,
+		},
+		slug: {
+			type: String,
+			unique: true,
+			index: 1,
+			required: true,
+		},
+		description: {
+			required: true,
+			maxLength: 5000,
+		},
+		quantity: {
+			type: Number,
+			required: true,
+			default: 0,
+		},
+		// images: []{},
+		thumbnailImage: {
+			type: String,
+			required: true,
+			default: '',
+		},
+		price: {
+			type: Number,
+			required: true,
+			default: 0,
+		},
+		salePrice: {
+			type: Number,
+			default: 0,
+		},
+		saleDate: {
+			type: Date,
+			default: null,
+		},
+		ratings: {
+			type: Number,
+			max: 5,
+			default: 5,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+export default mongoose.model('Product', productSchema);
