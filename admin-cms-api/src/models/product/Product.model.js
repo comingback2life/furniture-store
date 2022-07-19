3e978214a85edf85f82034fb8a970c556f43acdb
Samip <callmesamip@gmail.com>
2022-07-19 11:02:46 +1000

    Models done

---
+++ admin-cms-api/src/models/product/Product.model.js
@@ -0,0 +1,19 @@
+import ProductSchema from './Product.schema.js';
+
+export const insertProduct = (dataObj) => {
+	return ProductSchema(dataObj).save();
+};
+
+export const getProduct = (filter) => {
+	return ProductSchema.findOne(filter);
+};
+
+export const getMultipleProducts = (filter) => {
+	return ProductSchema.find(filter);
+};
+export const updateProduct = (filter, updateObj) => {
+	return ProductSchema.findOneAndUpdate(filter, updateObj, { new: true });
+};
+export const deleteProduct = (filter) => {
+	return ProductSchema.findOneAndDelete(filter);
+};
