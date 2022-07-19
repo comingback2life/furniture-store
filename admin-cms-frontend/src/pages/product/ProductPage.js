2e7e03df24201ab5df73eee157f9f504b48b96e5
Samip <callmesamip@gmail.com>
2022-07-19 13:08:46 +1000

    Working on the product frontend

---
+++ admin-cms-frontend/src/pages/product/ProductPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import AdminLayout from '../layouts/AdminLayout.js';
+
+const ProductPage = () => {
+	return <AdminLayout>Product Page</AdminLayout>;
+};
+export default ProductPage;
