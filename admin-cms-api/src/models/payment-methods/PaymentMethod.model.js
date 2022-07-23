02b6d50e02203fd327a44807c266106cece3e737
Samip <callmesamip@gmail.com>
2022-07-23 11:47:37 +1000

    Working on Payment Methods

---
+++ admin-cms-api/src/models/payment-methods/PaymentMethod.model.js
@@ -0,0 +1,26 @@
+import PaymentMethodSchema from './PaymentMethod.schema.js';
+
+export const insertPaymentMethod = (obj) => {
+	return PaymentMethodSchema(obj).save();
+};
+
+export const getCategories = (filter) => {
+	return PaymentMethodSchema.find(filter);
+};
+
+export const getAllPaymentMethods = () => {
+	//for admin to provide unfiltered results.
+	return PaymentMethodSchema.find();
+};
+export const getOnePaymentMethod = (filter) => {
+	return PaymentMethodSchema.findOne(filter);
+};
+
+export const deletePaymentMethodById = (_id) => {
+	return PaymentMethodSchema.findByIdAndDelete(_id);
+};
+
+//@updateObj must be an obj and ID should be a string
+export const updatePaymentMethod = (_id, updateObj) => {
+	return PaymentMethodSchema.findByIdAndUpdate(_id, updateObj, { new: true });
+};
