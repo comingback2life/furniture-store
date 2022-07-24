953b453d5d8a3e6388dec73e376d35542f113881
Samip <callmesamip@gmail.com>
2022-07-24 22:32:23 +1000

    Working on Payment Router API

--- admin-cms-api/src/middlewares/joi-validations/paymentValidation.js
+++ admin-cms-api/src/middlewares/joi-validations/paymentValidation.js
@@ -3,6 +3,7 @@ import { SHORTSTR, LONGSTR, validator } from './constantValidators.js';
 
 export const newPaymentMethodValidation = (req, res, next) => {
 	const schema = Joi.object({
+		_id: LONGSTR.allow(null, ''),
 		status: SHORTSTR.required(),
 		name: SHORTSTR.required(),
 		description: LONGSTR.required(),
