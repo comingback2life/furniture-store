c4951b80e62f35ef206c92771bbce39ad4a2a970
Samip <callmesamip@gmail.com>
2022-08-02 20:42:20 +1000

    Adding token to the session db

--- admin-cms-api/src/helpers/jwtHelper.js
+++ admin-cms-api/src/helpers/jwtHelper.js
@@ -1,7 +1,15 @@
 import jwt from 'jsonwebtoken';
-
+import { insertSession } from './../models/session/Session.model';
 //payload must be an object
 
-export const signJWT = (payload) => {
-	jwt.sign(payload, process.env.JWT_ACCESS_SECRET);
+export const signAccessJWT = (payload) => {
+	const accessJWT = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
+		expiresIn: '15m',
+	});
+	return insertSession(accessJWT);
+};
+
+export const verifyAccessJWT = (jwtToken) => {
+	return jwt.verify(toke, process.env.JWT_ACCESS_SECRET);
+	//send token and type to the session Table
 };
