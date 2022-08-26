4c2c2d64e4fc203cbac4c18137af5af2386bac9c
Samip <callmesamip@gmail.com>
2022-08-26 22:08:21 +1000

    Removed unused console log

--- admin-cms-api/src/routers/adminRouter.js
+++ admin-cms-api/src/routers/adminRouter.js
@@ -318,7 +318,6 @@ router.get('/accessjwt', async (req, res, next) => {
 		const refreshJWT = req.headers.authorization;
 		console.log(refreshJWT);
 		const decodedJWT = verifyRefreshJWT(refreshJWT);
-		console.log('=================', decodedJWT);
 		if (decodedJWT?.email) {
 			const user = await getAdmin({ email: decodedJWT.email, refreshJWT });
 			if (user?._id) {
