7b6b499f839993c326ba5e6b2548ce6758161dde
Samip <callmesamip@gmail.com>
2022-08-29 23:51:43 +1000

    Playing around with the styles, changed the header text

--- admin-cms-frontend/src/pages/dashboard-page/Dashboard.js
+++ admin-cms-frontend/src/pages/dashboard-page/Dashboard.js
@@ -2,29 +2,53 @@ import React from 'react';
 import CustomCard from '../../components/custom-card/CustomCard';
 import AdminLayout from '../layouts/AdminLayout';
 import { FcPackage, FcApproval, FcHighPriority } from 'react-icons/fc';
+import { Col, Row } from 'react-bootstrap';
 export const Dashboard = () => {
 	return (
 		<AdminLayout>
-			<span className="lead fw-bold">Product Summary</span>
-			<div className="productInfo d-flex mt-3 justify-content-around">
-				<CustomCard title="Total Products" count="555" logo={<FcPackage />} />
-				<CustomCard title="Active Products" count="400" logo={<FcApproval />} />
-				<CustomCard
-					title="Inactive Products"
-					count="400"
-					logo={<FcHighPriority />}
-				/>
-			</div>
-			<span className="lead fw-bold">Product Summary</span>
-			<div className="productInfo d-flex mt-3">
-				<CustomCard title="Total Products" count="555" logo={<FcPackage />} />
-				<CustomCard title="Active Products" count="400" logo={<FcApproval />} />
-				<CustomCard
-					title="Inactive Products"
-					count="400"
-					logo={<FcHighPriority />}
-				/>
-			</div>
+			<Row>
+				<p>Dashboard</p>
+			</Row>
+			<Row>
+				<Col>
+					<span className="lead fw-bold">Product Summary</span>
+					<div className="productInfo d-flex mt-3">
+						<CustomCard
+							title="Total Products"
+							count="555"
+							logo={<FcPackage />}
+						/>
+						<CustomCard
+							title="Active Products"
+							count="400"
+							logo={<FcApproval />}
+						/>
+						<CustomCard
+							title="Inactive Products"
+							count="400"
+							logo={<FcHighPriority />}
+						/>
+					</div>
+					<span className="lead fw-bold">Product Summary</span>
+					<div className="productInfo d-flex mt-3">
+						<CustomCard
+							title="Total Products"
+							count="555"
+							logo={<FcPackage />}
+						/>
+						<CustomCard
+							title="Active Products"
+							count="400"
+							logo={<FcApproval />}
+						/>
+						<CustomCard
+							title="Inactive Products"
+							count="400"
+							logo={<FcHighPriority />}
+						/>
+					</div>
+				</Col>
+			</Row>
 		</AdminLayout>
 	);
 };
--- admin-cms-frontend/src/pages/layouts/Header.js
+++ admin-cms-frontend/src/pages/layouts/Header.js
@@ -26,7 +26,7 @@ export const Header = () => {
 					)}
 
 					<LinkContainer to="/">
-						<Navbar.Brand>Furniture Store Admin</Navbar.Brand>
+						<Navbar.Brand>eCommerce Admin</Navbar.Brand>
 					</LinkContainer>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
