3f31447984cd478e048fa4629f38d360ed1f3afd
Samip <callmesamip@gmail.com>
2022-08-29 21:01:34 +1000

    Started building the dashboard

--- admin-cms-frontend/src/pages/dashboard-page/Dashboard.js
+++ admin-cms-frontend/src/pages/dashboard-page/Dashboard.js
@@ -3,5 +3,9 @@ import AdminLayout from '../layouts/AdminLayout';
 import { DefaultLayout } from '../layouts/DefaultLayout';
 
 export const Dashboard = () => {
-	return <AdminLayout>Dashboard</AdminLayout>;
+	return (
+		<AdminLayout>
+			<div className="productInfo">Product Information Section</div>
+		</AdminLayout>
+	);
 };
