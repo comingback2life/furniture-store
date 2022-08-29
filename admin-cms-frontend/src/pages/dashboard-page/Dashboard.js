77d66a91068ebf9c49c28d6424ac7b3cd87b0f53
Samip <callmesamip@gmail.com>
2022-08-29 23:20:46 +1000

    Added the card CSS and integrated into the dashboard

--- admin-cms-frontend/src/pages/dashboard-page/Dashboard.js
+++ admin-cms-frontend/src/pages/dashboard-page/Dashboard.js
@@ -2,21 +2,23 @@ import React from 'react';
 import CustomCard from '../../components/custom-card/CustomCard';
 import AdminLayout from '../layouts/AdminLayout';
 import { DefaultLayout } from '../layouts/DefaultLayout';
-
+import { FcPackage } from 'react-icons/fc';
 export const Dashboard = () => {
 	return (
 		<AdminLayout>
 			<h4 className="py-3">Dashboard</h4>
 			<span className="lead">Product Summary</span>
-			<div className="productInfo d-flex">
-				<CustomCard>555</CustomCard>
-				<CustomCard />
+			<div className="productInfo d-flex mt-3">
+				<CustomCard title="Total Products" count="555" logo={<FcPackage />} />
+				<CustomCard title="Active Products" count="400" />
+				<CustomCard title="Inactive Products" count="400" />
 			</div>
-			<div className="user-info">
-				<CustomCard title="User Information" />
+			<span className="lead">Product Summary</span>
+			<div className="productInfo d-flex mt-3">
+				<CustomCard title="Total Products" count="555" logo={<FcPackage />} />
+				<CustomCard title="Active Products" count="400" />
+				<CustomCard title="Inactive Products" count="400" />
 			</div>
-			<div className="last-orders">Last 5 orders</div>
-			<div className="top-5">Top 5 Products </div>
 		</AdminLayout>
 	);
 };
