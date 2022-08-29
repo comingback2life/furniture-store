ae2a7592772694e11d516953d0c64f446db26350
Samip <callmesamip@gmail.com>
2022-08-29 23:31:58 +1000

    Hopefully completed the styling

--- admin-cms-frontend/src/components/custom-card/CustomCard.js
+++ admin-cms-frontend/src/components/custom-card/CustomCard.js
@@ -3,9 +3,10 @@ import React from 'react';
 import './customCard.css';
 const CustomCard = ({ title, count, logo }) => {
 	return (
-		<div className="isCard mb-3">
-			<div className="isLogo">{logo}</div>
-			<span className="ms-auto">{title}</span>
+		<div className="isCard mt-3 mb-3">
+			{logo ? <div className="isLogo">{logo}</div> : <></>}
+			<p className="ms-auto fw-bold">{title}</p>
+			<p className="ms-auto isCardCount">{count}</p>
 		</div>
 	);
 };
--- admin-cms-frontend/src/pages/dashboard-page/Dashboard.js
+++ admin-cms-frontend/src/pages/dashboard-page/Dashboard.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import CustomCard from '../../components/custom-card/CustomCard';
 import AdminLayout from '../layouts/AdminLayout';
-import { DefaultLayout } from '../layouts/DefaultLayout';
-import { FcPackage } from 'react-icons/fc';
+import { FcPackage, FcApproval, FcHighPriority } from 'react-icons/fc';
 export const Dashboard = () => {
 	return (
 		<AdminLayout>
-			<h4 className="py-3">Dashboard</h4>
-			<span className="lead">Product Summary</span>
-			<div className="productInfo d-flex mt-3">
+			<span className="lead fw-bold">Product Summary</span>
+			<div className="productInfo d-flex mt-3 justify-content-around">
 				<CustomCard title="Total Products" count="555" logo={<FcPackage />} />
-				<CustomCard title="Active Products" count="400" />
-				<CustomCard title="Inactive Products" count="400" />
+				<CustomCard title="Active Products" count="400" logo={<FcApproval />} />
+				<CustomCard
+					title="Inactive Products"
+					count="400"
+					logo={<FcHighPriority />}
+				/>
 			</div>
-			<span className="lead">Product Summary</span>
+			<span className="lead fw-bold">Product Summary</span>
 			<div className="productInfo d-flex mt-3">
 				<CustomCard title="Total Products" count="555" logo={<FcPackage />} />
-				<CustomCard title="Active Products" count="400" />
-				<CustomCard title="Inactive Products" count="400" />
+				<CustomCard title="Active Products" count="400" logo={<FcApproval />} />
+				<CustomCard
+					title="Inactive Products"
+					count="400"
+					logo={<FcHighPriority />}
+				/>
 			</div>
 		</AdminLayout>
 	);
