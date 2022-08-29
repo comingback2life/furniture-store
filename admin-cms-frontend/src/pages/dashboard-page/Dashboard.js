1f91801e2557af96a7b1c0db1cffeeff105d2123
Samip <callmesamip@gmail.com>
2022-08-29 22:23:10 +1000

    Styling the dashboard

--- admin-cms-frontend/src/pages/dashboard-page/Dashboard.js
+++ admin-cms-frontend/src/pages/dashboard-page/Dashboard.js
@@ -6,6 +6,9 @@ export const Dashboard = () => {
 	return (
 		<AdminLayout>
 			<div className="productInfo">Product Information Section</div>
+			<div className="user-info">User Information</div>
+			<div className="last-orders">Last 5 orders</div>
+			<div className="top-5">Top 5 Products </div>
 		</AdminLayout>
 	);
 };
